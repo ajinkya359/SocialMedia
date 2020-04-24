4bdbf4fd43a6846df6524d68e31ff4405653197f
ajinkya359 <sajinkya359@gmail.com>
2020-04-24 18:10:19 +0530

    added to windows

--- Server/app.js
+++ Server/app.js
@@ -1,28 +1,36 @@
-const express=require('express')
-const app=express();
-const cors=require('cors');
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const session=require('express-session');
-const bodyParser=require('body-parser');
+const session = require("express-session");
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(express.static('../public'));
-app.set('views engine','ejs');
-app.set('views','../views');
+app.use(express.static("../public"));
+app.set("views engine", "ejs");
+app.set("views", "../views");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({
-    secret:'seCRet',
-    resave:false,
-    saveUninitialized:true,
-    cookie:{maxAge:3600000}// This time is in millisecond.
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(
+  session({
+    secret: "seCRet",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 3600000,
+    }, // This time is in millisecond.
+  })
+);
 
-app.use('/',require('../routes/index.js'))
+app.use("/", require("../routes/index.js"));
 
-app.use('/users',require('../routes/users.js'))
-app.get("*",(req,res)=>{
-    res.status(404).send('You did something wrong');
-})
-const PORT=3000;
-app.listen(PORT,console.log(`Server has started on ${PORT}`));
+app.use("/users", require("../routes/users.js"));
+app.get("*", (req, res) => {
+  res.status(404).send("You did something wrong");
+});
+const PORT = 3000;
+app.listen(PORT, console.log(`Server has started on ${PORT}`));
