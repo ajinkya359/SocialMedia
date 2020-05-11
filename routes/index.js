afcd4ee631032064466a319970eb915082487537
ajinkya359 <sajinkya359@gmail.com>
2020-05-11 16:23:33 +0530

    added the user id in people

--- routes/index.js
+++ routes/index.js
@@ -4,35 +4,40 @@ const mySqlConnection = require("../Database/database")
 
 router.get('/', (req, res) => res.status(200).send('home-page'));
 
-router.get('/dashboard',(req,res)=>{
-    if(req.session.user)
-        res.render("dashboard.ejs",{name:req.session.user.name,image:req.session.user.image});
-        // res.status(200).send(req.session.user);
+router.get('/dashboard', (req, res) => {
+    if (req.session.user)
+        res.render("dashboard.ejs", {
+            name: req.session.user.name,
+            image: req.session.user.image
+        });
+    // res.status(200).send(req.session.user);
     else
         res.status(401).send("You are not logged in");
 })
-router.get('/chat',(req,res)=>{
-    if(req.session.user)
-    {
-       res.render("chat.ejs",{name:req.session.user.name});
-    }
-    else{
+router.get('/chat', (req, res) => {
+    if (req.session.user) {
+        res.render("chat.ejs", {
+            name: req.session.user.name
+        });
+    } else {
         res.status(404).send("Sorry But you need to login first");
     }
 })
-router.get('/people',(req,res)=>{
-     mySqlConnection.query(
-         "select * from users",
-         (err,rows)=>{
-             if(err) res.send(err);
-             else {
-                 var users=[]
-                 rows.forEach(user => {
-                     users.push(user.name);
-                 });
-                 res.render('people.ejs',{h:users})
-             }  
-         }
-     )
+router.get('/people', (req, res) => {
+    mySqlConnection.query(
+        "select * from users",
+        (err, rows) => {
+            if (err) res.send(err);
+            else {
+                var users = []
+                rows.forEach(user => {
+                    users.push([user.name, user.id]);
+                });
+                res.render('people.ejs', {
+                    h: users
+                })
+            }
+        }
+    )
 })
 module.exports = router;
