cb235a77a3fe82547ce34c11717aa860dbb4b78a
ajinkya359 <sajinkya359@gmail.com>
2020-05-13 05:43:18 +0530

    added friends table in /Database/tables.sql

---
+++ Database/tables.sql
@@ -0,0 +1,18 @@
+
+create table friends
+(
+    id int
+    auto_increment primary key,
+     name varchar
+    (255), 
+     email varchar
+    (255), 
+     phone char
+    (10), 
+     relationship varchar
+    (255), 
+     userID int, 
+     FOREIGN KEY
+    (userID) REFERENCES users
+    (id)
+);
